b4bccc5b651e9c97ee57e7ccf8983304b0a42a6d
Yoendi <yoendigy1408@gmail.com>
2025-01-22 20:55:26 -0500

    Adding musica de fondo

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,19 @@
-<script setup></script>
+<script setup>
+
+import { onMounted } from "vue";
+
+const playAudio = async () => {
+  try {
+    const sound = await import("@/assets/audio/musicadefondo.mp3");
+    const audio = new Audio(sound.default);
+    audio.play();
+  } catch (error) {
+    console.error("Error al cargar o reproducir el audio:", error);
+  }
+};
+
+onMounted(playAudio);
+</script>
 
 <template>
   <RouterView />
